7d349d87d5b629b56f8b5fcfaa84a6583d969364
Savcab <felixchenyijun@gmail.com>
2023-04-30 20:14:31 -0700

    added option to select token pair

--- front-end/src/pages/StakeForm.tsx
+++ front-end/src/pages/StakeForm.tsx
@@ -7,6 +7,12 @@ export default function StakeForm() {
         <h2 className={styles.title}>Choose Amount</h2>
         <div className={styles.field}>
           <input type="number" id="amount" className={styles.input} />
+          <select id="token-pair" className={styles.input}>
+            <option value="ETH/USDT">ETH/USDT</option>
+            <option value="WBTC/ETH">WBTC/ETH</option>
+            <option value="USDC/ETH">USDC/ETH</option>
+            <option value="LINK/ETH">LINK/ETH</option>
+          </select>
         </div>
         <button type="submit" className={styles.button}>
           Stake
